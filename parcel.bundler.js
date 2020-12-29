36fe1c40ea32cb62089fcfcd9f0e062c53ba7227
Tyler Williams <tyler.williams@spothero.com>
2020-12-29 11:01:40 -0500

    initial structure

---
+++ parcel.bundler.js
@@ -0,0 +1,27 @@
+const merge = require("deepmerge");
+const Bundler = require("parcel-bundler");
+
+const isDevMode = process.env.WATCH === "true";
+
+const options = {
+  hmr: false,
+  hmrPort: 0,
+  logLevel: 3,
+  cache: false,
+  target: "node",
+  watch: isDevMode,
+  scopeHoist: false,
+  sourceMaps: false,
+  autoInstall: true,
+  minify: !isDevMode,
+  contentHash: false,
+  detailedReport: true,
+  bundleNodeModules: "create-component",
+};
+
+(async function () {
+  await new Bundler(
+    "./src/index.js",
+    merge(options, { outDir: "./bin", outFile: "cli.js" })
+  ).bundle();
+})();
